ff909561940b330fc5aafe5821f626049657aeb1
AbdallhElzayat2020 <https://github.com/AbdallhElzayat2020/E-commerce-React-App>
2023-12-04 04:30:35 +0200

    filter

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Title from "./components/Title";
+import menu from "./data";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  // const [filter, setFilter] = useState();
+  return (
+    <main className="menu">
+      <Title text="Our Menu" />
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </main>
+  );
+};
+export default App;
